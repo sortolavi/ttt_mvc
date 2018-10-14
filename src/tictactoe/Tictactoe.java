52057ae041c0a0eb9bdfdfedc8ad660927e98c44
Timo Sorakivi <timo.sorakivi@gmail.com>
2018-10-14 19:13:22 +0300

    added args support

--- src/tictactoe/Tictactoe.java
+++ src/tictactoe/Tictactoe.java
@@ -4,6 +4,7 @@ import model.Game;
 import view.Board;
 import model.ai.*;
 import controller.Controller;
+import java.util.Arrays;
 
 import javax.swing.*;
 /**
@@ -16,14 +17,20 @@ public class Tictactoe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         int whoStarts = JOptionPane.showConfirmDialog(null, "Opponent moves first?", "Start the game",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         boolean oppStart = (whoStarts == JOptionPane.YES_OPTION)? true : false;
         
         Game game = new Game();
         Board board = new Board();
         MoveStrategy ms = new Minimax();
-//        MoveStrategy ms = new Casual();
+        
+        if (args.length != 0) {
+            switch (args[0]){
+            case "Casual":
+                ms = new Casual();
+            }
+        }
 
         Controller controller = new Controller(board, game, ms);
         board.addEventListener(controller);
